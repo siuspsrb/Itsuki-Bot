2326844bf6b8c56b6e02812af96f445dd62a2402
— hyzer xcx. <98345064+siuspsrb@users.noreply.github.com>
2025-06-24 21:21:30 +0700

    Update total.js

--- cmd/info/total.js
+++ cmd/info/total.js
@@ -1,22 +1,27 @@
 commands.add({
     name: ["totalcommands", "totalfitur"],
     command: ["totalcommands", "totalfitur"],
-    category: "info",
     alias: ["totalcmd"],
+    category: "info",
     desc: "menampilkan total semua fitur yang tersedia",
     run: async ({ sius, m }) => {
         const allCmds = commands.getAllCommands({ onlyEnabled: true })
         const grouped = {}
+        let total = 0
+
         for (const cmd of allCmds) {
             const cat = cmd.category.toUpperCase()
             if (!grouped[cat]) grouped[cat] = []
             grouped[cat].push(cmd)
+            total += cmd.command.length
         }
-        const total = allCmds.length
-        let teks = `*TOTAL FITUR AKTIF: ${total}*\n\n`
+
+        let teks = `*TOTAL COMMAND AKTIF: ${total}*\n\n`
         for (const [cat, list] of Object.entries(grouped).sort()) {
-            teks += `▢ ${cat}: ${list.length} Fitur\n`
+            const jumlah = list.reduce((acc, x) => acc + x.command.length, 0)
+            teks += `▢ ${cat}: ${jumlah} Trigger\n`
         }
+
         sius.reply(m.chat, teks.trim(), "S T A T I S T I K  -  F I T U R", false)
     }
 })
